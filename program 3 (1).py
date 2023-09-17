36687af449222e16c6b8c3aeb0e5ae9f3435030a
Purva <103106609+purvaad@users.noreply.github.com>
2023-09-17 19:25:07 +0530

    Update program 3 (1).py

--- program 3 (1).py
+++ program 3 (1).py
@@ -6,7 +6,7 @@ max = arr[0]
 
 for i in range(len(arr)):
     if arr[i]  < min : min = arr[i]
-if arr[i] > max : max = arr[i]
+    if arr[i] > max : max = arr[i]
 
 print("Maximum is: ", max)
 print("Minimum is: ", min)
